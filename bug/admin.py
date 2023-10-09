d48d65352e40f4112aa026f9c51bee44e425a73e
spykelionel <ndilionel6@gmail.com>
2023-10-09 11:52:06 +0100

    creat model and make migrations

--- bug/admin.py
+++ bug/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from bug.models import Bug
 
-# Register your models here.
+admin.site.register(Bug)
--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Bug(models.Model):
+    BUG_TYPES = (
+        ('error', 'Error'),
+        ('new_feature', 'New Feature'),
+    )
+
+    STATUS_CHOICES = (
+        ('todo', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    )
+
+    description = models.TextField()
+    bug_type = models.CharField(max_length=20, choices=BUG_TYPES)
+    report_date = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
